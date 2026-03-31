3fc366be2ef70f054cf0e279cee1404a0e3e8d7a
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-31 22:45:25 +0300

    Added card 2

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ window.addEventListener('scroll', function() {
         header.style.backdropFilter = 'none';
     }
 }); 
-
-/*let index = 0;
-const slides = document.querySelector(".slides");
-const total = document.querySelectorAll(".slides img").length;
-
-function showSlide(i){
-  slides.style.transform = `translateX(-${i * 100}%)`;
-}
-
-setInterval(()=>{
-  index = (index + 1) % total;
-  showSlide(index);
-},3000);*/
 let index = 0;
 const slides = document.querySelector(".slides");
 const dots = document.querySelectorAll(".dots span");
@@ -39,40 +26,7 @@ dots.forEach((dot, i)=>{
     showSlide(index);
   });
 });
-/*let index = 0;
-const slider = document.querySelector(".slider");
-const slides = document.querySelector(".slides");
-const total = document.querySelectorAll(".slides img").length;
 
-let startX = 0;
-let endX = 0;
-
-function showSlide(i){
-  slides.style.transform = `translateX(-${i * 100}%)`;
-}
-
-// TOUCH START
-slider.addEventListener("touchstart", (e)=>{
-  startX = e.touches[0].clientX;
-});
-
-// TOUCH END
-slider.addEventListener("touchend", (e)=>{
-  endX = e.changedTouches[0].clientX;
-
-  let diff = startX - endX;
-
-  if(diff > 50){
-    // swipe left
-    index = (index + 1) % total;
-  }
-  else if(diff < -50){
-    // swipe right
-    index = (index - 1 + total) % total;
-  }
-
-  showSlide(index);
-});*/
 
 // show first slide
 showSlide(index);
@@ -103,61 +57,6 @@ function nextSlide(){
   showSlide(index);
 }
 
-// stop auto when user interacts
 dots.forEach(dot=>{
   dot.addEventListener("click", ()=> clearInterval(auto));
 });
-/*let index = 0;
-
-const slider = document.querySelector(".slider");
-const slides = document.querySelector(".slides");
-const images = document.querySelectorAll(".slides img");
-const dots = document.querySelectorAll(".dots span");
-
-const total = images.length;
-
-let startX = 0;
-let endX = 0;
-
-// SHOW SLIDE FUNCTION
-function showSlide(i){
-  slides.style.transform = `translateX(-${i * 100}%)`;
-
-  // update dots
-  dots.forEach(dot => dot.classList.remove("active"));
-  dots[i].classList.add("active");
-}
-
-// DOT CLICK
-dots.forEach((dot, i)=>{
-  dot.addEventListener("click", ()=>{
-    index = i;
-    showSlide(index);
-  });
-});
-
-// TOUCH START
-slider.addEventListener("touchstart", (e)=>{
-  startX = e.touches[0].clientX;
-});
-
-// TOUCH END
-slider.addEventListener("touchend", (e)=>{
-  endX = e.changedTouches[0].clientX;
-
-  let diff = startX - endX;
-
-  if(diff > 50){
-    // swipe left
-    index = (index + 1) % total;
-  }
-  else if(diff < -50){
-    // swipe right
-    index = (index - 1 + total) % total;
-  }
-
-  showSlide(index);
-});
-
-// INITIAL LOAD
-showSlide(index);*/
